0715f9ab88fa5eb0d0d5e33d3b22b231a7a99df4
Robenatrading <robena.trading@yahoo.com>
2026-07-29 16:38:16 +0300

    Backup inainte de reorganizarea produselor

---
+++ src/data/produse.js
@@ -0,0 +1,101 @@
+export const produse = [
+  {
+    nume: "Pat medical",
+    slug: "pat-medical",
+    categorie: "mobilier-medical",
+    categorieNume: "Mobilier medical",
+    imagine: "/images/pat-medical-robena-trading.png",
+    descriere:
+      "Pat medical destinat spitalelor, clinicilor, centrelor medicale și unităților de îngrijire. Produsul poate fi configurat și personalizat în funcție de cerințele beneficiarului.",
+  },
+
+  {
+    nume: "Noptieră medicală",
+    slug: "noptiera-medicala",
+    categorie: "mobilier-medical",
+    categorieNume: "Mobilier medical",
+    imagine: "/images/noptiera-medicala-robena-trading.png",
+    descriere:
+      "Noptieră medicală destinată unităților sanitare și centrelor de îngrijire, disponibilă în diferite variante și configurații.",
+  },
+
+  {
+    nume: "Masă instrumentar inox",
+    slug: "masa-instrumentar-inox",
+    categorie: "mobilier-medical",
+    categorieNume: "Mobilier medical",
+    imagine: "/images/masa-instrumentar-inox-robena-trading.png",
+    descriere:
+      "Masă pentru instrumentar realizată din inox, destinată spațiilor medicale și disponibilă în diferite dimensiuni și configurații.",
+  },
+
+  {
+    nume: "Dulap metalic",
+    slug: "dulap-metalic",
+    categorie: "mobilier-metalic",
+    categorieNume: "Mobilier metalic",
+    imagine: "/images/dulap-metalic-gri-robena-trading.png",
+    descriere:
+      "Dulap metalic disponibil în diferite dimensiuni, culori și configurații, adaptat cerințelor beneficiarului.",
+  },
+
+  {
+    nume: "Raft metalic",
+    slug: "raft-metalic",
+    categorie: "mobilier-metalic",
+    categorieNume: "Mobilier metalic",
+    imagine: "/images/raft-metalic-robena-trading.png",
+    descriere:
+      "Raft metalic destinat depozitării și organizării produselor, disponibil în diferite dimensiuni și configurații.",
+  },
+
+  {
+    nume: "Vestiar metalic",
+    slug: "vestiar-metalic",
+    categorie: "mobilier-metalic",
+    categorieNume: "Mobilier metalic",
+    imagine: "/images/vestiar-metalic-robena-trading.png",
+    descriere:
+      "Vestiar metalic personalizabil, disponibil cu unul sau mai multe compartimente și diferite configurații interioare.",
+  },
+
+{
+  nume: "Birou profesional",
+  slug: "birou-profesional",
+  categorie: "mobilier-de-birou",
+  categorieNume: "Mobilier de birou",
+  imagine: "/images/categorii/mobilier-birou-robena-trading.png",
+  descriere:
+    "Birou profesional disponibil în diferite dimensiuni, finisaje și configurații, adaptat fiecărui spațiu.",
+},
+
+  {
+    nume: "Dulap de birou",
+    slug: "dulap-birou",
+    categorie: "mobilier-de-birou",
+    categorieNume: "Mobilier de birou",
+    imagine: "/images/categorii/dulap-birou-robena-trading.png",
+    descriere:
+      "Dulap de birou disponibil în diferite dimensiuni, finisaje și configurații, cu posibilitatea personalizării.",
+  },
+
+  {
+    nume: "Scaun de birou",
+    slug: "scaun-birou",
+    categorie: "mobilier-de-birou",
+    categorieNume: "Mobilier de birou",
+    imagine: "/images/categorii/scaun-de-birou-robena-trading.png",
+    descriere:
+      "Scaun de birou disponibil în diferite modele, materiale și configurații, adaptat cerințelor beneficiarului.",
+  },
+
+  {
+    nume: "Concentrator de oxigen",
+    slug: "concentrator-oxigen",
+    categorie: "aparatura",
+    categorieNume: "Aparatură",
+    imagine: "/images/categorii/concentrator-de-oxigen-portabil-robena-trading.png",
+    descriere:
+      "Concentrator de oxigen destinat utilizării medicale, disponibil în diferite configurații și capacități.",
+  },
+];
