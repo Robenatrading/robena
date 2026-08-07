1546ac0e44b0e48ebdc50668df5b57eba391c8df
Robenatrading <robena.trading@yahoo.com>
2026-08-07 09:59:31 +0300

    Adauga sitemap pentru SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   site: "https://robena.ro",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
